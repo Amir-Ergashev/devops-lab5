1cf2c9684024d629958ad2af06da7c9fd9c63cd4
Amir-Ergashev <ergasevamir03@gmail.com>
2026-06-14 17:13:49 +0300

    fix: correct router prefix structure

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from src.routers.user import router as user
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(prefix="/v1")
 router.include_router(user, prefix="/user", tags=["User"])
