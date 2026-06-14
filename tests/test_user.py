fd4e4ba35d98dc268cd5d810c90a5939b033fc8a
Amir-Ergashev <ergasevamir03@gmail.com>
2026-06-14 11:56:48 +0300

    done lab 5

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,41 +1,39 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
 
 # Существующие пользователи
 users = [
-    {
-        'id': 1,
-        'name': 'Ivan Ivanov',
-        'email': 'i.i.ivanov@mail.com',
-    },
-    {
-        'id': 2,
-        'name': 'Petr Petrov',
-        'email': 'p.p.petrov@mail.com',
-    }
+    {"id": 1, "name": "Ivan Ivanov", "email": "i.i.ivanov@mail.com"},
+    {"id": 2, "name": "Petr Petrov", "email": "p.p.petrov@mail.com"}
 ]
 
 def test_get_existed_user():
-    '''Получение существующего пользователя'''
-    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    """Получение существующего пользователя"""
+    response = client.get("/api/v1/user", params={"email": users[0]["email"]})
     assert response.status_code == 200
     assert response.json() == users[0]
 
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
-    pass
+    """Получение несуществующего пользователя"""
+    response = client.get("/api/v1/user", params={"email": "notexist@mail.com"})
+    assert response.status_code == 404
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
+    """Создание пользователя с уникальной почтой"""
+    new_user = {"name": "Sidor Sidorov", "email": "s.sidorov@mail.com"}
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    """Создание пользователя с почтой, которую использует другой пользователь"""
+    duplicate_user = {"name": "New Ivan", "email": users[0]["email"]}
+    response = client.post("/api/v1/user", json=duplicate_user)
+    assert response.status_code == 409
 
 def test_delete_user():
-    '''Удаление пользователя'''
-    pass
+    """Удаление пользователя"""
+    response = client.delete("/api/v1/user", params={"email": users[1]["email"]})
+    assert response.status_code == 204
